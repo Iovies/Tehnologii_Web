6f2b95f6c5d6c35950470078551ea7c55be73aec
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2023-04-13 07:33:12 +0300

    TicketController

---
+++ SMCS/SMCS.Web/Controllers/TicketControllers/TicketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMCS.Web.Controllers.TicketControllers
+{
+    public class TicketController : SMCSController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
