4c547d8cfb23f766f76ab2316700ef12d461144d
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2023-04-13 03:09:24 +0300

    BL Services: TicketService

---
+++ SMCS/SMCS.BusinessLogic/Services/TicketService.cs
@@ -0,0 +1,14 @@
+﻿using SMCS.BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMCS.BusinessLogic.Services
+{
+    public class TicketService : ITicketService
+    {
+       
+    }
+}
