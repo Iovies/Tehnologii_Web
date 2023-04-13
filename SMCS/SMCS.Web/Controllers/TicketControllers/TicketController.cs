cb1e92570eaeea1c733db78a07c83881b6711177
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2023-04-13 17:18:55 +0300

    Presentation: TicketController -> In line Ticket declaration

--- SMCS/SMCS.Web/Controllers/TicketControllers/TicketController.cs
+++ SMCS/SMCS.Web/Controllers/TicketControllers/TicketController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SMCS.Domains.Entities;
+using SMCS.Web.Models.Tickets;
 
 namespace SMCS.Web.Controllers.TicketControllers
 {
     public class TicketController : SMCSController
     {
+        [HttpGet]
+        [Route("ticket/index")]
         public IActionResult Index()
         {
-            return View();
+            var model = new ViewTicketsViewModel();
+            model.Tickets = new List<Ticket>()
+            {
+                new Ticket() {TicketNumber = "755636",TicketTitle="Test ticket" },
+                new Ticket() {TicketNumber = "987498",TicketTitle="Other test ticket" },
+                new Ticket() {TicketNumber = "812377",TicketTitle="Another test ticket" },
+                new Ticket() {TicketNumber = "234552",TicketTitle="Some test ticket" },
+                new Ticket() {TicketNumber = "328383",TicketTitle="New test ticket" },
+            };
+            return View(model);
+        }
+        [HttpGet]
+        [Route("ticket/viewticket/{id}")]
+        public IActionResult ViewTicket(int id)
+        {
+            return View(id);
         }
     }
 }
