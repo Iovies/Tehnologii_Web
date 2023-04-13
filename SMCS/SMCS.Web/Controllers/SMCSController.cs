acbec5453d63bd874ba8b8d20f9284e3253c4d85
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2023-04-13 07:32:56 +0300

    Presentation: Base Controller

---
+++ SMCS/SMCS.Web/Controllers/SMCSController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMCS.Web.Controllers
+{
+    public class SMCSController : Controller
+    {
+        private readonly ISession _session;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
