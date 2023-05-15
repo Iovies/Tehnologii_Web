2fb74d858c8316e36559bb30a9ab3f21c7195f7c
Dumitru Vartic <104945638+DumitruVartic@users.noreply.github.com>
2023-05-15 21:13:11 +0300

    Added Chat
    Added Basic Chat Template

---
+++ SMCS/SMCS.Web/Controllers/ChatControllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMCS.Web.Controllers.ChatControllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
