bd13e35056a45bfc65f78602bc666dac49c90426
Dumitru Vartic <104945638+DumitruVartic@users.noreply.github.com>
2023-05-15 23:57:58 +0300

    Implemented/Hardcoded Basic Models for Chat

--- SMCS/SMCS.Web/Controllers/ChatControllers/ChatController.cs
+++ SMCS/SMCS.Web/Controllers/ChatControllers/ChatController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SMCS.Web.Models.Chat;
+using SMCS.Web.Models.Lodging;
 
 namespace SMCS.Web.Controllers.ChatControllers
 {
@@ -6,7 +8,44 @@ namespace SMCS.Web.Controllers.ChatControllers
     {
         public IActionResult Index()
         {
-            return View();
+            /*
+                The idea is this -> we send a list of chats (List<Chat>) in view
+                when the user chooses one of the chats from the list, then in _chat its model is used to display the list of messages Chat.messageList[0-over9999] (messageList<message>)
+                message must contain information about the type of message (incoming or outgoing) in order to mark the respective message
+            */
+            var model = new Chats();
+            model.chats = new List<Chat>()
+            {
+                new Chat() {
+                    Receiver="Ion",
+                    Sender="NotIon",
+                    messages = new List<Message>()
+                    { // Username = Receiver >??
+                        new Message() {Username="Ion", MType=Message.MessageType.Outgoing, Body="Hello World from Ion"},
+                        new Message() {Username="NotIon", MType=Message.MessageType.Incoming, Body="Hello to you too"}
+                    }
+                },
+                new Chat() {
+                    Receiver="Sanea",
+                    Sender="Gheorhe",
+                    messages = new List<Message>()
+                    {
+                        new Message() {Username="Sanea", MType=Message.MessageType.Incoming, Body="Hello World from Sanea"},
+                        new Message() {Username="Gheorhe", MType=Message.MessageType.Outgoing, Body="Hello To you Gheorhe"}
+                    }
+                },
+                new Chat() {
+                    Receiver="Ion",
+                    Sender="Feghea",
+                    messages = new List<Message>()
+                    {
+                        new Message() {Username="Ion", MType=Message.MessageType.Outgoing, Body="Maine Ore nu avem"},
+                        new Message() {Username="Feghea", MType=Message.MessageType.Incoming, Body="De ce?"}
+                    }
+                }
+            };
+
+            return View(model);
         }
     }
 }
---
+++ SMCS/SMCS.Web/Models/Chat/Message.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace SMCS.Web.Models.Chat
+{
+    public class Message
+    {
+        /*public int Id { get; set; } // ? */
+        public string Username { get; set; } // Name of the sender of the message (In general can be gained from chat model, but if this is a group chat then it is nedeed)
+        public string Body { get; set; } // Text of the message
+
+        /*
+            It can be done differently :
+            If we are doing not group chat then simple check if the message username is the same as chat Username of the person -> then it's incoming message ?
+        */
+        
+        // test implementation
+        public string MessageClass { get { return MType == MessageType.Incoming ? "incoming-chat" : "outgoing-chat"; } set { MessageClass = "incoming-chat"; } }
+
+        public MessageType MType { get; set; }
+        public enum MessageType
+        {
+            Incoming,
+            Outgoing
+        }
+    }
+}
