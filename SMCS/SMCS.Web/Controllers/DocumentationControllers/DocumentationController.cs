8d4079c72ca50154c6feb01a4e39683f1985fa46
Dumitru Vartic <104945638+DumitruVartic@users.noreply.github.com>
2023-05-17 00:25:57 +0300

    Fix: Collapse, Scroll. See details
    -Fixed minimized sidebar toggler
    -Fixed minimized chat toggler
    -Fixed scrolling regions for chat messages & list of chats

---
+++ SMCS/SMCS.Web/Controllers/DocumentationControllers/DocumentationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMCS.Web.Controllers.DocumentationControllers
+{
+    public class DocumentationController : Controller
+    {
+        [HttpGet]
+        [Route("docs")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
