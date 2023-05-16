062664d3e62b672dd687ca4a0ee132c619394961
Dumitru Vartic <104945638+DumitruVartic@users.noreply.github.com>
2023-05-17 01:42:22 +0300

    Added Dark Theme (Test Version) - DOM Strorage
    -Added Dark Style
    -The Style is persistent across pages
    -It is stored it local storage (Dom) cookie
    -Known Issue : Both light and dark styles loading on each pages each time

---
+++ SMCS/SMCS.Web/wwwroot/js/style-toggle-dom.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var selectedStyle = localStorage.getItem('selectedStyle');
+    var darkStyle = document.getElementById('darkStyle');
+    var defaultStyle = document.getElementById('defaultStyle');
+
+    if (selectedStyle === 'dark') {
+        darkStyle.disabled = false;
+        defaultStyle.disabled = true;
+    } else {
+        darkStyle.disabled = true;
+        defaultStyle.disabled = false;
+    }
+});
---
+++ SMCS/SMCS.Web/wwwroot/js/style-toggle.js
@@ -0,0 +1,14 @@
+﻿function toggleStyle() {
+    var darkStyle = document.getElementById('darkStyle');
+    var defaultStyle = document.getElementById('defaultStyle');
+
+    if (darkStyle.disabled) {
+        darkStyle.disabled = false;
+        defaultStyle.disabled = true;
+        localStorage.setItem('selectedStyle', 'dark');
+    } else {
+        darkStyle.disabled = true;
+        defaultStyle.disabled = false;
+        localStorage.setItem('selectedStyle', 'default');
+    }
+}
