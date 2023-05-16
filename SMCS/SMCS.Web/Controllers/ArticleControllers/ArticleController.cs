92354ecd7c38ec5f2d0f99055651d8c060ab1654
Iulik <westovies@gmail.com>
2023-05-16 21:26:54 +0300

    Add Article Views

---
+++ SMCS/SMCS.Web/Controllers/ArticleControllers/ArticleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMCS.Web.Controllers.ArticleControllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
